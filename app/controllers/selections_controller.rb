3ba13f8d455b014cc59b55214b995f437ef27604
habib mboup <7habibiz@gmail.com>
2020-03-04 13:30:58 -0500

    Testing params

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -1,35 +1,27 @@
 class SelectionsController < ApplicationController
 
   def index
-    @selections = Selection.all
-    if (params[:selection] && activities = Activity.where(activity_id: params[:selection]))
-      activities = activities
-      @selection = Selection.where(id: params[:selection])
+
+    # @selections = Selection.all
+    @category = params[:categories]
+    @interest = params[:interest]
+    if (@category || @interest)
+
+      @activities = Activity.joins(:pairs).where(pairs:{category_id: @category.id.to_i}, interest: @interest)
+      # @activites.each do |activity|
+        #unless user.selections.find { |selection| selection.activity_id == activity.id }
+          # Selection.create(activity_id: activity.id, user_id: current_user.id)
+        #end
+      #end
     else
-      @activities = Activity.all
+     @activities = Activity.all
     end
-  end
-
-  def create
-    # create a new selection
-    # @selection = Selection.new
-    # # find the category
-    # @category = Category.where(name: params[:category])
-    # # find all the pairs with category.id
-    # @pairs = Pair.where(category_id: @category)
-    # raise
-    # redirect_to selections_path
-
-
-    # @activity = Activity.where(interest: params[:interest])
-    # # @selection.categories = @category
-    # # assign an activity_id to @selection
-    # @selection.activity_id = @activity
-    # @selection.save
-  end
-
-  def selections_params
-
+    # if (params[:selection] && activities = Activity.where(activity_id: params[:selection]))
+    #   activities = activities
+    #   @selection = Selection.where(id: params[:selection])
+    # else
+    #   @activities = Activity.all
+    # end
   end
 
 end
