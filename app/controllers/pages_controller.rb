5f41cc87b4f39ca99ef8fcfce901450cb21ff9b0
augustin brun brun.aub1512@gmail.com <brun.aub1512@gmail.com>
2020-03-04 11:46:00 -0500

    create a form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    # @categories = Category.all
-    # @activities = Activity.all
+    @categories = Category.all
+    @activities = Activity.all
+    @pairs = Pair.all
   end
 end
--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -5,21 +5,22 @@ class SelectionsController < ApplicationController
   end
 
   def create
-    # create a new selection
-    # @selection = Selection.new
-    # # find the category
-    # @category = Category.where(name: params[:category])
-    # # find all the pairs with category.id
-    # @pairs = Pair.where(category_id: @category)
-    # raise
-    # redirect_to selections_path
+    create a new selection
+    @selection = Selection.new
+    # find the category
+    @category = Category.where(name: params[:category])
+    # find all the pairs with category.id
+    @pairs = Pair.where(category_id: @category)
+    raise
+    redirect_to selections_path
 
 
-    # @activity = Activity.where(interest: params[:interest])
-    # # @selection.categories = @category
-    # # assign an activity_id to @selection
-    # @selection.activity_id = @activity
-    # @selection.save
+    @activity = Activity.where(interest: params[:interest])
+    # @selection.categories = @category
+    # assign an activity_id to @selection
+    @selection.activity_id = @activity
+    @selection.save
+    raise
   end
 
 end
